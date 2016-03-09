f3bb7d11959e9ca957cda91be75fec47726bcf36
Pavel <mamontov.pavel@gmail.com>
2016-03-09 18:04:27 -0500

    Still working on the styleguide

--- app/styleguide/views.py
+++ app/styleguide/views.py
@@ -20,3 +20,23 @@ def typography():
 @mod.route('/styleguide/forms')
 def forms():
     return render_template('styleguide/forms.html')
+
+@mod.route('/styleguide/tables')
+def tables():
+    return render_template('styleguide/tables.html')
+
+@mod.route('/styleguide/alerts')
+def alerts():
+    return render_template('styleguide/alerts.html')
+
+@mod.route('/styleguide/utilities')
+def utilities():
+    return render_template('styleguide/utilities.html')
+
+@mod.route('/styleguide/media_queries')
+def media_queries():
+    return render_template('styleguide/media_queries.html')
+
+@mod.route('/styleguide/templating')
+def templating():
+    return render_template('styleguide/templating.html')
