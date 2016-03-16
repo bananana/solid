9ccd4831ac245dcf6e639b29c9f51be23bcb6b1c
Pavel <mamontov.pavel@gmail.com>
2016-03-16 16:58:59 -0400

    Wrote some unit tests for users module

---
+++ tests_base.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+from config import basedir
+from app import app, db
+
+
+class BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
