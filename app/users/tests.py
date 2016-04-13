7a28aa369d1e115b17a8cfe90072c06efb95bcd8
Pavel Mamontov <mamontov.pavel@gmail.com>
2016-04-13 16:34:31 -0400

    Made user unit tests more thorough. Also, now using Flask-Testing.

--- app/users/tests.py
+++ app/users/tests.py
@@ -2,6 +2,7 @@ from flask import url_for
 from flask.ext.login import current_user, login_user, logout_user
 from app.tests_base import BaseTestCase
 from app.users.models import User
+from app.causes.models import Cause
 
 class UserViewsTests(BaseTestCase):
     
@@ -138,93 +139,11 @@ class UserViewsTests(BaseTestCase):
            q = User.query.filter_by(nickname='Tester').first()
            self.assertIsNotNone(q)
 
-
-'''
-from app import bcrypt
-from app.tests_base import BaseTestCase
-from app.users.models import User
-from app.causes.models import Cause
-from flask.ext.login import current_user
-#from flask import url_for
-
-class UserViewsTests(BaseTestCase):
-    #: Dummy user profile to be recycled by tests
-    test_user = {
-        'nickname'  : 'Tester',
-        'password'  : bcrypt.generate_password_hash('test'),
-        'full_name' : 'Test Testov',
-        'email'     : 'test@test.com',
-        'phone'     : 1234567890,
-        'zip'       : 12345,
-        'employer'  : 'Boss'
-    }
-
-    #: Dummpy cause to be recycled by tests
-    test_cause = { 
-        'title'     : 'Test Cause',
-        'slug'      : 'test_cause',
-        'boss'      : 'Boss'
-    }
-
-    def signup(self, context, email, nickname, password, verify_password):
-        return context.post('/signup', data={
-            'email'           : email,
-            'nickname'        : nickname,
-            'password'        : password,
-            'verify_password' : verify_password
-        }, follow_redirects=True)
-
-    def login(self, context, email, password):
-        return context.post('/login', data={
-            'email'    : email,
-            'password' : password,
-            'remember' : 'n'
-        }, follow_redirects=True)
-
-    def logout(self):
-        return self.app.get('/logout', follow_redirects=True)
-    
-    def edit(self, context, nickname, fields):
-        return context.post('/user/' + nickname + '/edit', data=fields)
-
-    def test_user_signup(self):
-        with app.test_client() as c:
-            self.signup(c, 'test.signup@test.com', 'SignupTest', 'test', 'test')
-            self.assertTrue(current_user.is_authenticated)
-            self.assertFalse(current_user.is_anonymous)
-            self.logout()
-
-    def test_user_login(self):
-        User.create(**self.test_user)
-        with app.test_client() as c:
-            self.login(c, 'test@test.com', 'test')
-            self.assertTrue(current_user.is_authenticated)
-            self.assertFalse(current_user.is_anonymous)
-            self.assertEqual(current_user.nickname, 'Tester')
-            self.assertEqual(current_user.email, 'test@test.com')
-
-    def test_user_logout(self):
-        with app.test_client() as c:
-            self.login(c, 'test@test.com', 'test')
-            self.logout()
-            # For some reason assertIsNone() doesn't work
-            self.assertEqual(current_user, None)
-
-    def test_user_delete(self):
-        User.create(**self.test_user)
-        with app.test_client() as c:
-            self.login(c, 'test@test.com', 'test')
-            return c.get('/user/' + current_user.nickname + '/delete')
-            user = User.querty.filter_by(nickname='Tester').first()
-            self.assertEqual(user, None)
-            self.assertFalse(current_user.is_authenticated)
-            self.assertTrue(current_user.is_anonymous)
-            
     def test_user_edit(self):
-        User.create(**self.test_user)
-        with app.test_client() as c:
-            self.login(c, 'test@test.com', 'test')
-            self.edit(c, 'Tester', fields={
+        u = User.create(**self.test_user)
+        with self.client: 
+            login_user(u) 
+            return self.client.post('/user/' + u.nickname + '/edit', data={
                 'nickname'    : 'Tester2',
                 'full_name'   : 'Different Name',
                 'email'       : 'different.email@test.com',
@@ -241,23 +160,55 @@ class UserViewsTests(BaseTestCase):
             self.assertEqual(current_user.employer, 'Another Employer')
             self.assertEqual(current_user.description, 'Test description')
 
+    def test_user_edit_fail(self):
+        u = User.create(**self.test_user)
+        u2 = User.create(**self.test_user_2)
+        with self.client: 
+            login_user(u) 
+            return self.client.post('/user/' + u2.nickname + '/edit', data={
+                'nickname'    : 'Tester2',
+                'full_name'   : 'Different Name',
+                'email'       : 'different.email@test.com',
+                'phone'       : 9876543210,
+                'zip'         : 54321,
+                'employer'    : 'Another Employer'
+            })
+            self.assertEqual(u2.nickname, 'Tester')
+            self.assertEqual(u2.full_name, 'Test Testov')
+            self.assertEqual(u2.email, 'test@test.com')
+            self.assertEqual(u2.phone, 1234567890)
+            self.assertEqual(u2.zip, 12345)
+            self.assertEqual(u2.employer, 'Boss')
+
+    def test_user_password_setting(self):
+        u = User.create(**self.test_user)
+        with self.client:
+            u.set_password('test')
+            self.assertTrue(u.is_valid_password('test'))
+
+    def test_user_password_setting_fail(self):
+        u = User.create(**self.test_user)
+        with self.client:
+            u.set_password('test')
+            self.assertFalse(u.is_valid_password('tset'))
+
+
     def test_user_initials_generation(self):
-        User.create(**self.test_user)
-        with app.test_client() as c:
-            self.login(c, 'test@test.com', 'test')
+        u = User.create(**self.test_user)
+        with self.client:
+            login_user(u)
             current_user.generate_initials()
-            self.assertTrue(current_user.initials, 'IT')
+            self.assertTrue(current_user.initials, 'TT')
 
     def test_user_cause_support(self):
-        user = User.create(**self.test_user)
-        cause = Cause.create(**self.test_cause)
-        user.support(cause)
-        self.assertTrue(user.is_supporting(cause))
+        u= User.create(**self.test_user)
+        c= Cause.create(**self.test_cause)
+        u.support(c)
+        self.assertTrue(u.is_supporting(c))
 
     def test_user_cause_unsupport(self):
-        user = User.create(**self.test_user)
-        cause = Cause.create(**self.test_cause)
-        user.support(cause)
-        user.unsupport(cause) 
-        self.assertFalse(user.is_supporting(cause))
-'''
+        u = User.create(**self.test_user)
+        c = Cause.create(**self.test_cause)
+        u.support(c)
+        u.unsupport(c) 
+        self.assertFalse(u.is_supporting(c))
