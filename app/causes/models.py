c7a5f9b59fe7be44abdc04b484201d98a5f5706d
Carl van Tonder <carl@supervacuo.com>
2016-12-08 17:49:15 +0000

    Fix supporting action counting

--- app/causes/models.py
+++ app/causes/models.py
@@ -65,7 +65,9 @@ class Cause(CRUDMixin, db.Model):
     def count_action_supports(self):
         from app.users.models import User
         query = action_supporters.count(
-            whereclause=User.id.in_(a.id for a in self.actions.all())
+            whereclause=action_supporters.c.action_id.in_(
+                a.id for a in self.actions.all()
+            )
         )
         result = db.engine.execute(query)
         return result.fetchone()[0]
