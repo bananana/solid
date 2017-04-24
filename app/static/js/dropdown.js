d15b18088983e34f768ca9ff357895deaadf8f53
Pavel Mamontov <mamontov.pavel@gmail.com>
2017-04-24 17:49:56 -0400

    Reworked dropdowns. Added a main menu to save space in header.

--- app/static/js/dropdown.js
+++ app/static/js/dropdown.js
@@ -7,4 +7,8 @@ $(document).ready(function () {
         $('#lang-dropdown > .dropdown-button').toggleClass('dropdown-button-click');
         $('#lang-dropdown > .dropdown-content').toggleClass('dropdown-content-click');
     });
+    $('#main-menu').click(function() {
+        $('#main-menu > .dropdown-button').toggleClass('dropdown-button-click');
+        $('#main-menu > .dropdown-content').toggleClass('dropdown-content-click');
+    });
 });
