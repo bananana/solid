79ccc877ec7c1d66b81fa2cdeb13609b760b7f84
Carl van Tonder <carl@supervacuo.com>
2017-08-22 13:08:20 -0400

    Switch to WTForms-Alchemy, user-uploadable images

--- app/causes/forms.py
+++ app/causes/forms.py
@@ -1,52 +1,56 @@
 from flask_babel import lazy_gettext as _
-from flask_wtf import Form 
-from wtforms.ext.sqlalchemy.orm import model_form
+from flask_wtf import Form
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
 
 from app import db
 
 from .models import Cause, CauseTranslation, Action, ActionTranslation
 
 
-CauseForm = model_form(Cause, base_class=Form, db_session=db.session, only=(
-	'boss',
-	'location',
-	'image',
-), field_args={
-    'location': {'label': _('Location') },
-    'boss': {'label': _('Boss') },
-    'image': {'label': _('Image') },
-})
+from wtforms_alchemy import model_form_factory
+ModelForm = model_form_factory(Form)
 
-CauseTranslationForm = model_form(
-	CauseTranslation, base_class=Form, db_session=db.session, only=(
-		'title',
-		'intro',
-		'story_heading',
-		'story_content'
-	), field_args={
-		'title': {'label': _('Title') },
-		'intro': {'label': _('Intro') },
-		'story_heading': {'label': _('Heading') },
-		'story_content': {'label': _('Body') },
-	}
-)
 
-ActionForm = model_form(Action, base_class=Form, db_session=db.session, only=(
-	'expiration',
-	'link',
-), field_args={
-    'expiration': {'label': _('Expiration') },
-    'link': {'label': _('Link') },
-})
+class CauseForm(ModelForm):
+	image = FileField()
 
-ActionTranslationForm = model_form(
-	ActionTranslation, base_class=Form, db_session=db.session, only=(
-		'title',
-		'summary',
-		'description',
-	), field_args={
-		'title': {'label': _('Title') },
-		'summary': {'label': _('Summary') },
-		'description': {'label': _('Description') },
-	}
-)
+	class Meta:
+		model = Cause
+		only = ('boss', 'location')
+		field_args = {
+			'location': {'label': _('Location') },
+			'boss': {'label': _('Boss') },
+			'image': {'label': _('Image') },
+		}
+
+class CauseTranslationForm(ModelForm):
+	class Meta:
+		model = CauseTranslation
+		only = ('title', 'intro', 'story_heading', 'story_content')
+		field_args = {
+			'title': {'label': _('Title') },
+			'intro': {'label': _('Intro') },
+			'story_heading': {'label': _('Heading') },
+			'story_content': {'label': _('Body') },
+		}
+
+
+class ActionForm(ModelForm):
+	class Meta:
+		model = Action
+		only = ('expiration', 'link',)
+		field_args = {
+			'expiration': {'label': _('Expiration') },
+			'link': {'label': _('Link') },
+		}
+
+class ActionTranslationForm(ModelForm):
+	class Meta:
+		model = ActionTranslation
+		only = ('title', 'summary', 'description',)
+		field_args = {
+			'title': {'label': _('Title') },
+			'summary': {'label': _('Summary') },
+			'description': {'label': _('Description') },
+		}
