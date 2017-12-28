13b43203649550841aa88fb27f7b588e0e459bd4
Carl van Tonder <carl@supervacuo.com>
2017-12-28 01:04:11 -0500

    Fix BCC email sending

--- app/email.py
+++ app/email.py
@@ -7,9 +7,9 @@ from flask_mailgun.message import Message
 from app import app, mailgun
 
 
-def _send_async_email(app, msg):
-    with app.app_context():
-        r = mailgun.send(msg)
+def _send_async_email(_app, msg):
+    with _app.app_context():
+        mailgun.send(msg)
 
 
 def send_email(subject, recipients, context, template_text, template_html=None, sender=None, bcc=None):
@@ -19,12 +19,12 @@ def send_email(subject, recipients, context, template_text, template_html=None, 
 
     if len(recipients) > 1:
         bcc = list(recipients)
-        recipients = [app.config['DEFAULT_EMAIL_SENDER'],]
+        recipients = [app.config['DEFAULT_EMAIL_SENDER'], ]
+
+    msg = Message(subject, recipients=recipients, bcc=bcc)
 
     if sender is not None:
         msg.sender = sender
-
-    msg = Message(subject, sender=sender, recipients=recipients, bcc=bcc)
 
     msg.body = render_template(template_text, **context)
 
